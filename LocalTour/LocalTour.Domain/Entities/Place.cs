1b3d63a9127fdf75a54d1a2b37d9be20f9dd76c8
HDuc85 <hoangduc8501@gmail.com>
2024-10-22 08:04:17 +0700

    Update Entities Place
    Add a column "Status"

--- LocalTour/LocalTour.Domain/Entities/Place.cs
+++ LocalTour/LocalTour.Domain/Entities/Place.cs
@@ -19,6 +19,8 @@ public partial class Place
 
     public double Latitude { get; set; }
 
+    public string Status { get; set; }
+
     public virtual ICollection<Destination> Destinations { get; set; } = new List<Destination>();
 
     public virtual ICollection<Event> Events { get; set; } = new List<Event>();
