9517af2acaa65bdb6f14d3aa9af63c11866952f8
HDuc85 <hoangduc8501@gmail.com>
2024-11-12 13:19:05 +0700

    Update PlaceReport and UserReport Entity

--- LocalTour/LocalTour.Domain/Entities/PlaceReport.cs
+++ LocalTour/LocalTour.Domain/Entities/PlaceReport.cs
@@ -6,14 +6,12 @@ namespace LocalTour.Domain.Entities;
 public partial class PlaceReport
 {
     public int Id { get; set; }
-    
     public Guid UserReportId { get; set; }
-
     public int PlaceId { get; set; }
-
     public DateTime ReportDate { get; set; }
-
     public string Status { get; set; } = null!;
+    
+    public string Content { get; set; } = null!;
 
     public virtual User UserReport { get; set; } = null!;
     public virtual Place Place { get; set; } = null!;
