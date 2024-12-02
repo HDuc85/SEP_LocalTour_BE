15c8bf8769a6cecb072f9f1dd08ecc08b64d8d93
Yu282002 <yoshino291@gmail.com>
2024-12-03 03:54:42 +0700

    update User
    add GetUserList
    add UnBan User

--- LocalTour/LocalTour.Services/ViewModel/GetUserRequest.cs
+++ LocalTour/LocalTour.Services/ViewModel/GetUserRequest.cs
@@ -10,22 +10,16 @@ namespace LocalTour.Services.ViewModel
     public class GetUserRequest
     {
         public Guid Id { get; set; }
-        public string FullName { get; set; }
+        public string? Username { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+        public string? Gender { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public string? Address { get; set; }
-        public string? Gender { get; set; }
-        public string? ProfilePictureUrl { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime DateUpdated { get; set; }
-        public DateTime? LastLoginDate { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public bool EmailConfirmed { get; set; }
-        public string? PhoneNumber { get; set; }
-        public bool PhoneNumberConfirmed { get; set; }
-        public bool TwoFactorEnabled { get; set; }
-        public bool LockoutEnabled { get; set; }
-        public int AccessFailedCount { get; set; }
         public List<string> Roles { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set;}
     }
 }
